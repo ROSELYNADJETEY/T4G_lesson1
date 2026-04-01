7d8e57f3c65ead40061f3299475b24d0e8ee9b24
ROSELYNADJETEY <roselynmingle386@gamil.com>
2026-04-01 07:13:17 -0700

    addition

---
+++ src/script.js
@@ -0,0 +1,37 @@
+// alerting to the user
+alert("hello,dear💞");
+let x=2;
+
+
+// let age;//declaring a variable
+age=8;//initializing a variable
+let name="mingle";//declaring and initializing a variable
+// alert(age);
+// alert("hello"name)
+// let name="Tech4Girls";
+
+
+// age=10;//reassigning the value of age from 8 to 10
+//alert(age);//Expecting to alert 10
+
+
+//const age1=15;
+//alert(age1);
+
+
+//age1=21;//This will throw an error because we cannot reassign a value to a constant variable
+//alert(age1)
+
+//prompt("What is your name?");//This will prompt the user to enter their name
+
+let userName=prompt("What is your name?");//This will prompt the user to enter their name and store it in the variable userName
+alert("Hello " + userName + ",❤ 💫 💛 welcome to Tech4Girls!" + "  🤝 we are glad to see you here" + " 🤝");//This will alert a greeting message to the user using the value stored in userName//concatenation action
+
+
+alert(`hello ${userName}. welcome to Tech4Girls!${age} years old`);//This will alert a greeting message to the user using template literals and the value stored in userName
+
+
+let sententce ="I"+ "am " + "here.";
+alert(sententce);//This will alert the concatenated string "I am here."
+
+
